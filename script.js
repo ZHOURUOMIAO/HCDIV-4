4a8410c5011adfc0f7361e5a545c074ef9c6cf24
ZHOURUOMIAO <3168767622@QQ.COM>
2024-12-04 17:32:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,37 @@
-// Set up map
-const map = L.map('map').setView([1.3521, 103.8198], 11); // Coordinates for Singapore
+const map = L.map('map').setView([1.3521, 103.8198], 11);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-// Load GeoJSON map data (sgmap.json)
-d3.json('https://raw.githubusercontent.com/ZHOURUOMIAO/HCDIV-4/main/sgmap.json').then(geoData => {
-  // Load population data (population2023.csv)
-  d3.csv('https://raw.githubusercontent.com/ZHOURUOMIAO/HCDIV-4/main/population2023.csv').then(popData => {
-    // Process population data to create a lookup map
-    const populationMap = {};
-    popData.forEach(d => {
-      populationMap[d.Subzone] = +d.Population;  // Subzone as key, population as value
-    });
-
-    // Define color scale for population
-    const colorScale = d3.scaleLinear()
-      .domain([0, d3.max(popData, d => +d.Population)])
-      .range(["lightgreen", "darkgreen"]);
-
-    // Style function for GeoJSON features
-    function style(feature) {
-      const pop = populationMap[feature.properties.name] || 0;
-      return {
-        fillColor: colorScale(pop),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-      };
-    }
-
-    // Add GeoJSON layer to map
-    L.geoJSON(geoData, {
-      style: style,
-      onEachFeature: (feature, layer) => {
-        layer.bindPopup(`<h3>${feature.properties.name}</h3><p>Population: ${populationMap[feature.properties.name] || 'N/A'}</p>`);
-      }
+d3.json('https://raw.githubusercontent.com/ZHOURUOMIAO/HCDIV-4/main/sgmap.json').then(function(data) {
+    L.geoJSON(data, {
+        style: function (feature) {
+            let population = getPopulation(feature.properties.name);
+            return {
+                color: getColor(population),
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.7
+            };
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("Region: " + feature.properties.name + "<br>Population: " + getPopulation(feature.properties.name));
+        }
     }).addTo(map);
-  });
 });
 
+function getPopulation(region) {
+    const populationData = {
+        "Central": 120000, 
+        "East": 150000,
+        "North": 110000,
+        "West": 140000
+    };
+    return populationData[region] || 0;
+}
+
+function getColor(population) {
+    return population > 150000 ? '#800026' :
+           population > 120000 ? '#BD0026' :
+           population > 100000 ? '#E31A1C' :
+           '#FFEDA0';
+}
